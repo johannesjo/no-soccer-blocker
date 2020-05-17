d9fcbca8baf0acb3d7cbda74eb79bf522cf157e5
Johannes Millan <johannes.millan@gmail.com>
2020-05-17 22:04:16 +0200

    feat: add more default words

--- src/options.model.ts
+++ src/options.model.ts
@@ -5,7 +5,7 @@ export interface OptionsModel {
 }
 
 export const DEFAULT_OPTS: OptionsModel = {
-  words: ['Fußball', 'Bundesliga', 'Derby', 'Tabellenplatz', 'Geisterspiel', 'Spieltag', 'Schalke', 'FC Bayern', 'Wechsel-Poker', 'DFB', 'Hertha', 'FC ', 'Strafraum', 'Eintracht Frankfurt', 'Einzelkritik', 'Torjubel', 'Top-Spiel', 'ohne Fans', 'Konferenzticker', 'sehenswerte Einzelaktion', 'holt Punkt', 'Bayern München', 'Borussia Dortmund'].join(','),
+  words: ['Fußball', 'Bundesliga', 'Derby', 'Tabellenplatz', 'Geisterspiel', 'Spieltag', 'Schalke', 'FC Bayern', 'Wechsel-Poker', 'DFB', 'Hertha', 'FC ', 'Strafraum', 'Eintracht Frankfurt', 'Einzelkritik', 'Torjubel', 'Top-Spiel', 'ohne Fans', 'Konferenzticker', 'sehenswerte Einzelaktion', 'holt Punkt', 'Bayern München', 'Borussia Dortmund', 'Strafstoß', 'Ligaspiel', 'Union Berlin'].join(','),
   btnTxt: 'Langweilig',
   isJustRemove: false,
 };
